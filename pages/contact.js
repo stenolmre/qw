40ff35c8156cc87eaf7ebf4e8c44d21b48e823b4
Sten Olmre <stenolmre@icloud.com>
2020-10-10 18:15:41 +0300

    test env 2

--- pages/contact.js
+++ pages/contact.js
@@ -1,11 +1,23 @@
 import { Fragment } from 'react';
 import Head from 'next/head';
+import axios from 'axios';
 
-export default function Contact() {
+export default function Contact({ notes }) {
   return <Fragment>
     <Head>
       <title>qw - contact</title>
     </Head>
     <div>What is your request?</div>
+    <div>
+      {
+        notes.data.map(note => <h2 key={note._id}>{note.title}</h2>)
+      }
+    </div>
   </Fragment>
+}
+
+Contact.getInitialProps = async () => {
+  const { data } = await axios.get('/api/notes');
+
+  return { notes: data }
 }

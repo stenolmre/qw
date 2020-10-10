e58d3cb8f913c738d1b62565073dc10cb90808be
Sten Olmre <stenolmre@icloud.com>
2020-10-10 16:57:06 +0300

    second init

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,11 @@
+import { Fragment } from 'react';
+import Head from 'next/head';
+
 export default function Index() {
-  return <div>{process.env.GREETING}</div>
+  return <Fragment>
+    <Head>
+      <title>qw</title>
+    </Head>
+    <div>{process.env.GREETING}</div>
+  </Fragment>
 }

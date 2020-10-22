352dae305c1b1e80fbf25280e654aa0cfa7fe5d4
Sten Olmre <stenolmre@icloud.com>
2020-10-22 12:27:18 +0300

    test

--- components/mobilelanding.js
+++ components/mobilelanding.js
@@ -1,10 +1,54 @@
-
+import Link from 'next/link'
 
 export default function MobileLanding() {
-  return <div style={{color: 'black'}} className="mobile-landing">
-    <h1>Experiences in the spotlight</h1>
-    <div className="mobile-experience-preview">
-      <img src="särkitunturi.JPG" alt="hiking"/>
+  return <div className="mobile-landing">
+    <div className="mobile-landing-header">
+      <h1>Top rated picks</h1>
+      <Link href="/"><a>See all</a></Link>
+    </div>
+    <div className="mobile-experience-container">
+      <ExperienceCard name="Snowshoe Trekking" place="Kätkatunturi"/>
+      <ExperienceCard name="Aurora Hike" place="Levitunturi"/>
+    </div>
+    <div className="overflow" />
+    <div className="mobile-landing-header">
+      <h1>latest posts</h1>
+      <Link href="/"><a>See all</a></Link>
+    </div>
+    <div className="mobile-experience-container">
+      <ExperienceCard name="Snowshoe Trekking" place="Kätkatunturi"/>
+      <ExperienceCard name="Aurora Hike" place="Levitunturi"/>
+    </div>
+    <br/>
+    <div className="mobile-landing-header">
+      <h1>Gallery</h1>
+      <Link href="/"><a>See all</a></Link>
+    </div>
+    <ImageSlider/>
+  </div>
+}
+
+function ExperienceCard({ name, place }) {
+  return <div className="mobile-experience-preview">
+    <img src="särkitunturi.JPG" alt="hiking"/>
+    <div className="mobile-overlay"/>
+    <div className="mobile-experience-info">
+      <h3>{ name }</h3>
+      <p><i className="fas fa-map-marker-alt"/> { place }</p>
+    </div>
+  </div>
+}
+
+function ImageSlider() {
+  return <div className="image-slider">
+    <div className="image-slider-img">
+      <img src="särkitunturi.JPG" alt="" />
+    </div>
+    <div className="image-slider-img">
+      <img src="särkitunturi.JPG" alt="" />
+    </div>
+    <div className="image-slider-img">
+      <img src="särkitunturi.JPG" alt="" />
     </div>
   </div>
 }

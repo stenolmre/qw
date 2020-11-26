5ff0461f197791977c644f9b23d064056a4dc0f3
Sten Olmre <stenolmre@icloud.com>
2020-11-26 16:30:16 +0200

    test

--- components/mobile/gallery.js
+++ components/mobile/gallery.js
@@ -1,15 +1,36 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import Link from 'next/link'
 import Heading from './../utils/heading'
 import gallery from './../arrays/gallery'
+import { useAlbumState, useAlbumDispatch } from './../../context/album'
+import { getAlbums } from './../../actions/album'
 
 export default function Gallery({ userLanguage }) {
+  const dispatchAlbum = useAlbumDispatch()
+  const albumState = useAlbumState()
+  const { albums } = albumState
+
+  useEffect(() => {
+    getAlbums(dispatchAlbum)
+  }, [dispatchAlbum])
+
   return <div className="mobile-section">
     <Heading name={userLanguage ? 'Gallery' : 'Galerii'} href="/albums" link={userLanguage ? 'see all' : 'vaata kõiki'}/>
     <div className="image-slider">
       {
-        gallery.map(image => <div key={image} className="image-slider-img">
-          <img src={image} alt="" />
-        </div>)
+        albumState && albums
+          ? albums.map(album => <Link key={album._id} href={`/albums/${album._id}`}><a>
+              <div className="album-preview">
+                {
+                  album.images.map(image => <div key={image} className="album-preview-image" style={{ backgroundImage: `url(${image})`}}/>).slice(0, 4)
+                }
+                <div className="album-overlay">
+                  <h2>+{album.images.length - 3}</h2>
+                </div>
+              </div>
+              <h4>{userLanguage ? album.name : album.nimi}</h4>
+            </a></Link>)
+          : <Spinner/>
       }
     </div>
   </div>

b72f9afb197955cd253c007ea2103478e6274b59
Sten Olmre <stenolmre@icloud.com>
2020-11-29 11:15:48 +0200

    test

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import Mobile from './../components/mobile/mobile'
 
 export default function Index() {
   return <Fragment>
-    <Head title="North Season" description="" image="https://etreeningud.ee/media/images/stenolmre/albums/K%C3%A4tk%C3%A4tunturi_11_2020/IMG_2946.jpg" url="https://stenolmre.com" />
+    <Head title="North Season - Travel to Inspire Your Life" description="Lapland. A land in the Arctic Circle with sweeping fells and northern lights, midnight sun and polar night. A home to reindeers, elves and Santa Claus, where for half a year, the trees wear winter coats. This could be the place where your next adventure awaits!" image="https://etreeningud.ee/media/images/stenolmre/OG_IMG_2946.jpg" url="https://stenolmre.com" />
     <div className="desktop">
       <Landing/>
     </div>

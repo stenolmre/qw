6fa67fd9ee1db98e7f02f8bbc6e218e22e68e7f1
Sten Olmre <stenolmre@icloud.com>
2020-11-29 11:06:37 +0200

    test

---
+++ components/utils/head.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Head from 'next/head'
+
+export default function MetaTags({ title, description, image, url }) {
+  return <Head>
+    <title>{title}</title>
+    <meta name="description" content={description}/>
+    <meta name="image" content={image}/>
+    <meta property="og:title" content={title}/>
+    <meta property="og:description" content={description}/>
+    <meta property="og:image" content={image}/>
+    <meta property="og:url" content={url}/>
+    <meta name="twitter:title" content={title}/>
+    <meta name="twitter:description" content={description}/>
+    <meta name="twitter:image" content={image}/>
+    <meta name="twitter:card" content="summary_large_image"/>
+  </Head>
+}
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,11 @@
 import React, { Fragment } from 'react'
-import Head from 'next/head'
+import Head from './../components/utils/head'
 import Landing from './../components/landing'
 import Mobile from './../components/mobile/mobile'
 
 export default function Index() {
   return <Fragment>
-    <Head>
-      <title>North Season</title>
-    </Head>
+    <Head title="North Season" description="" image="https://etreeningud.ee/media/images/stenolmre/albums/K%C3%A4tk%C3%A4tunturi_11_2020/IMG_2946.jpg" url="https://stenolmre.com" />
     <div className="desktop">
       <Landing/>
     </div>

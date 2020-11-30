a2d2885934f85a0ec455435a609b6f991416c369
Sten Olmre <stenolmre@icloud.com>
2020-11-30 11:54:38 +0200

    test

--- components/arrays/gallery.js
+++ components/arrays/gallery.js
@@ -1,12 +1,12 @@
 const gallery = [
-  'https://etreeningud.ee/media/images/stenolmre/albums/Front_Page/alta.jpeg',
-  'https://etreeningud.ee/media/images/stenolmre/albums/Front_Page/kätkätunturi.jpeg',
-  'https://etreeningud.ee/media/images/stenolmre/albums/Kätkätunturi_11_2020/IMG_2978.jpg',
-  'https://etreeningud.ee/media/images/stenolmre/albums/Front_Page/nordkapp.jpeg',
-  'https://etreeningud.ee/media/images/stenolmre/albums/Front_Page/särkitunturi.jpeg',
-  'https://etreeningud.ee/media/images/stenolmre/albums/Front_Page/särkitunturi_2.jpeg',
-  'https://etreeningud.ee/media/images/stenolmre/albums/Front_Page/skiing.jpeg',
-  'https://etreeningud.ee/media/images/stenolmre/albums/Kätkätunturi_11_2020/IMG_2937.jpg'
+  'https://etreeningud.ee/media/images/stenolmre/albums/Front_Page/IMG_0001.jpg',
+  'https://etreeningud.ee/media/images/stenolmre/albums/Front_Page/IMG_0002.jpg',
+  'https://etreeningud.ee/media/images/stenolmre/albums/Front_Page/IMG_0003.jpg',
+  'https://etreeningud.ee/media/images/stenolmre/albums/Front_Page/IMG_0004.jpg',
+  'https://etreeningud.ee/media/images/stenolmre/albums/Front_Page/IMG_0005.jpg',
+  'https://etreeningud.ee/media/images/stenolmre/albums/Front_Page/IMG_0006.jpg',
+  'https://etreeningud.ee/media/images/stenolmre/albums/Front_Page/IMG_0007.jpg',
+  'https://etreeningud.ee/media/images/stenolmre/albums/Front_Page/IMG_0008.jpg',
 ]
 
 export default gallery

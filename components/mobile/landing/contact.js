fcd66301da77bb1f85bca3263268679e0fee313d
Sten Olmre <stenolmre@icloud.com>
2020-12-19 22:36:02 +0200

    test

--- components/mobile/landing/contact.js
+++ components/mobile/landing/contact.js
@@ -1,33 +1,13 @@
 import React from 'react'
 import Link from 'next/link'
+import Info from './../contact/info'
 
 export default function MobileContact({ userLanguage }) {
   return <div className="mobile-contact-container">
     <div className="mobile-contact">
       <h2 className="mobile-landing-heading">CONTACT</h2>
         <br/>
-        <Link href="tel:37258553625"><a>
-          <div className="mobile-contact-detail">
-            <div className="mobile-contact-icon">
-              <i className="fas fa-phone"/>
-            </div>
-            <p>+372 5855 3625</p>
-          </div>
-        </a></Link>
-        <Link href="mailto:northseason@gmail.com"><a>
-          <div className="mobile-contact-detail">
-            <div className="mobile-contact-icon">
-              <i className="fas fa-envelope"/>
-            </div>
-            <p>northseason@gmail.com</p>
-          </div>
-        </a></Link>
-        <div className="mobile-contact-detail">
-          <div className="mobile-contact-icon">
-            <i className="fas fa-map-pin"/>
-          </div>
-          <p>Levi, Lapland</p>
-        </div>
+        <Info />
     </div>
   </div>
 }

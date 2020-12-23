02468a07d66e7fe63ee83243c55946da74e209a6
Sten Olmre <stenolmre@icloud.com>
2020-12-23 15:06:19 +0200

    test

--- pages/adventures/[id].js
+++ pages/adventures/[id].js
@@ -1,37 +1,33 @@
-import { Fragment, useEffect } from 'react'
+import React, { Fragment } from 'react'
 import cookies from 'next-cookies'
+import axios from 'axios'
 import Head from './../../components/utils/head'
-import { useRouter } from 'next/router'
 import MobileAdventure from './../../components/mobile/adventure/adventure'
-import { useAdventureState, useAdventureDispatch } from './../../context/adventure'
-import { getAdventure } from './../../actions/adventure'
 
-function Adventure({ language, person }) {
-  const dispatchAdventure = useAdventureDispatch()
-  const { adventure } = useAdventureState()
-  const { query } = useRouter()
+function Adventure({ language, adventure }) {
   const user_lang = language === 'eng' ? true : false
-  console.log(person);
-  useEffect(() => {
-    getAdventure(dispatchAdventure, query.id)
-  }, [dispatchAdventure, query])
 
   return <Fragment>
     {
-      adventure && <Head title={user_lang ? 'ss' : adventure.nimi} description={user_lang ? adventure.description : adventure.kirjeldus} image={adventure.images[0]} url={`https://stenolmre.com/adventures/${adventure._id}`}/>
+      adventure && <Head
+        title={user_lang ? adventure.name : adventure.nimi}
+        description={user_lang ? adventure.description : adventure.kirjeldus}
+        image={adventure.images[0]}
+        url={`https://stenolmre.com/adventures/${adventure._id}`}/>
     }
     <MobileAdventure userLanguage={user_lang}/>
   </Fragment>
 }
 
-Adventure.getInitialProps = ctx => {
+Adventure.getInitialProps = async ctx => {
   const { lan } = cookies(ctx)
+  const { id } = ctx.query
 
-  const person = !process.env.NODE_ENV || process.env.NODE_ENV === 'development'
-    ? 'Development'
-    : 'Sten Olmre'
+  const { data } = !process.env.NODE_ENV || process.env.NODE_ENV === 'development'
+    ? await axios.get(`http://localhost:3000/api/adventures/get?adventureId=${id}`)
+    : await axios.get(`https://stenolmre.com/api/adventures/get?adventureId=${id}`)
 
-  return { language: lan || '', person: person }
+  return { language: lan || '', adventure: data }
 }
 
 export default Adventure
--- pages/albums/[id].js
+++ pages/albums/[id].js
@@ -1,8 +1,8 @@
-import React, { Fragment, useEffect, useState } from 'react'
+import React, { Fragment, useState } from 'react'
 import axios from 'axios'
 import cookies from 'next-cookies'
+import Head from './../../components/utils/head'
 import MobileLayout from './../../components/mobile/layout'
-import Navbar from './../../components/mobile/navbar'
 import Slideshow from './../../components/utils/slideshow'
 
 function Album({ language, album }) {
@@ -11,6 +11,7 @@ function Album({ language, album }) {
   const user_lang = language === 'eng' ? true : false
 
   return <Fragment>
+    <Head title={user_lang ? album.name : album.nimi} description={user_lang ? album.info : album.infoEst} image={album.images[0]} url={`https://stenolmre.com/albums/${album._id}`} />
     <MobileLayout userLanguage={user_lang} heading={user_lang ? album.name : album.nimi} subheading={user_lang ? album.info : album.infoEst} id={album._id}>
       <div className="mobile-album-page-images">
       {
@@ -31,7 +32,9 @@ Album.getInitialProps = async ctx => {
   const { lan } = cookies(ctx)
   const { id } = ctx.query
 
-  const { data } = await axios.get(`https://stenolmre.com/api/albums/get?albumId=${id}`)
+  const { data } = !process.env.NODE_ENV || process.env.NODE_ENV === 'development'
+    ? await axios.get(`http://localhost:3000/api/albums/get?albumId=${id}`)
+    : await axios.get(`https://stenolmre.com/api/albums/get?albumId=${id}`)
 
   return { language: lan, album: data }
 }
